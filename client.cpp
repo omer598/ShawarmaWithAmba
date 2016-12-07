46e2749ef876e6d56faf40d4e5a622e3f556de1c
omer598 <omer598@users.noreply.github.com>
2016-12-08 01:35:20 +0200

    Latest

--- client.cpp
+++ client.cpp
@@ -17,6 +17,7 @@
 using namespace std;
 #define lines 6
 #define coloumns 7
+#define underline "\33[4m"
 
 struct sockaddr_serverIn{
 	short sin_family;
@@ -27,16 +28,16 @@ struct sockaddr_serverIn{
 
 void nullTheArray(char * buffer)
 {
-    for(int i=0;i<sizeof(buffer);i++)
-        buffer[i]='\0';
+	for (int i = 0; i<sizeof(buffer); i++)
+		buffer[i] = '\0';
 }
 
-void updateBoard(char* buffer,char** board)
+void updateBoard(char* buffer, char** board)
 {
-	int k=0;
-	for (int i=0;i<lines;i++)
+	int k = 0;
+	for (int i = 0; i<lines; i++)
 	{
-		for (int j=0;j<coloumns;j++)
+		for (int j = 0; j<coloumns; j++)
 		{
 			board[i][j] = buffer[k];
 			k++;
@@ -47,93 +48,97 @@ void updateBoard(char* buffer,char** board)
 
 void printBoard(char** msg)
 {
-	char sideBar='|';
-	char lowBar='_';
-	int k=0;
-	for(int i=0; i<lines; i++)
-	{
-		//cout<<sideBar;
-			cout<<sideBar;
-		for (int k = 0; k < coloumns*2; k++)
-			cout<<lowBar;
-		cout<<sideBar<<endl;
+	char sideBar = '|';
+	char lowBar = '_';
+	int k = 0;
 
-		cout<<sideBar;
+	cout << underline;
+	for (int k = 0; k <= coloumns * 2; k++)
+		cout << ' ';
+	cout << "\33[0m" << endl;
+
+	for (int i = 0; i<lines; i++)
+	{
+		cout << sideBar << underline;
 		for (int j = 0; j < coloumns; j++)
 		{
-			cout<<msg[i][j];
-			cout<<sideBar;
+			cout << msg[i][j];
+			if (j != (coloumns - 1))
+				cout << sideBar;
+
 		}
-		cout << endl;
+		cout << "\33[0m" << sideBar << endl;
 	}
+	/*
 	cout << sideBar;
-	for(int i=0;i<coloumns;i++)
-		cout<<lowBar;
+	for(int i=0;i<coloumns*2;i++)
+	cout<<lowBar;
 	cout<< sideBar <<endl;
+	*/
 }
 
 int main()
 {
-    int sock,portNum;
-    char buffer[1024]={'\0'};
-    char** board;
-    board = new char*[lines];
-    for (int p=0;p<lines;p++)
-            board[p] = new char[coloumns];
-    portNum=11111;
-    sock=socket(PF_INET,SOCK_STREAM,0);
-    if(sock<0)
-    {
-            cout <<"Error establish socket" <<endl;
-            return 0;
-    }
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons( 65535 );
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    connect(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in));
-    //Receives first massage (ip back!)
-    recv(sock,buffer,sizeof(buffer),0);
-    cout<<buffer<<endl;
-    recv(sock,buffer,sizeof(buffer),0);
-    cout<<buffer<<endl;
-    nullTheArray(buffer);
-    cin>>buffer;
-    send(sock,buffer,sizeof(buffer),0);
-    //recv(sock,buffer,sizeof(buffer),0);
-    while(buffer[0] != 'E')
-    {    
-        recv(sock,buffer,sizeof(buffer),0);
-        if(buffer[0] == 'I') //Player Color pick
-        {
-            recv(sock,buffer,sizeof(buffer),0);
-            cout << buffer << endl;
-            nullTheArray(buffer);
-            cin >> buffer;
-            send(sock,buffer,sizeof(buffer),0);
-            //recv(sock,buffer,sizeof(buffer),0);
-        }
-        else if (buffer[0] == '2') //Update board and update printed board
-        {
-            recv(sock,buffer,sizeof(buffer),0);
-            updateBoard(buffer, board);
-            printBoard(board);
-        }
-        else if (buffer[0] == 'M') //server massege
-        {
-            /*recives game info*/        
-            recv(sock,buffer,sizeof(buffer),0);
-            cout<< "Game info: "<<endl<<buffer<<endl;
-        }
-    }
-    /*TODO: insert xor for choose start player*/
+	int sock, portNum;
+	char buffer[1024] = { '\0' };
+	char** board;
+	board = new char*[lines];
+	for (int p = 0; p<lines; p++)
+		board[p] = new char[coloumns];
+	portNum = 11111;
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (sock<0)
+	{
+		cout << "Error establish socket" << endl;
+		return 0;
+	}
+	struct sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(65535);
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	connect(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in));
+	//Receives first massage (ip back!)
+	recv(sock, buffer, sizeof(buffer), 0);
+	cout << buffer << endl;
+	recv(sock, buffer, sizeof(buffer), 0);
+	cout << buffer << endl;
+	nullTheArray(buffer);
+	cin >> buffer;
+	send(sock, buffer, sizeof(buffer), 0);
+	//recv(sock,buffer,sizeof(buffer),0);
+	while (buffer[0] != 'E')
+	{
+		recv(sock, buffer, sizeof(buffer), 0);
+		if (buffer[0] == 'I') //Player Color pick
+		{
+			recv(sock, buffer, sizeof(buffer), 0);
+			cout << buffer << endl;
+			nullTheArray(buffer);
+			cin >> buffer;
+			send(sock, buffer, sizeof(buffer), 0);
+			//recv(sock,buffer,sizeof(buffer),0);
+		}
+		else if (buffer[0] == '2') //Update board and update printed board
+		{
+			recv(sock, buffer, sizeof(buffer), 0);
+			updateBoard(buffer, board);
+			printBoard(board);
+		}
+		else if (buffer[0] == 'M') //server massege
+		{
+			/*recives game info*/
+			recv(sock, buffer, sizeof(buffer), 0);
+			cout << "Game info: " << endl << buffer << endl;
+		}
+	}
+	/*TODO: insert xor for choose start player*/
 
-    //recv(sock,buffer,sizeof(buffer),0);
-    //cout<<endl<<buffer<<endl;
+	//recv(sock,buffer,sizeof(buffer),0);
+	//cout<<endl<<buffer<<endl;
 
-    cout << "The game is over." << endl;
-    close(sock);
-    cout << "Socket closed!" << endl;
-    return 0;
+	cout << "The game is over." << endl;
+	close(sock);
+	cout << "Socket closed!" << endl;
+	return 0;
 }
 
--- server.cpp
+++ server.cpp
@@ -12,6 +12,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <cstdio>
+#include <string.h>
 
 #define lines 6
 #define coloumns 7
@@ -25,148 +26,215 @@ struct sockaddr_serverIn{
 	char sin_zero[8];
 };
 struct user_data{
-        int client_id;
-        string username;
-        char color;
+	int client_id;
+	string username;
+	char color;
+	char start_bit;
 };
 void nullTheArray(char * buffer)
 {
-    for(int i=0;i<sizeof(buffer);i++)
-        buffer[i]='\0';
+	for (int i = 0; i<sizeof(buffer); i++)
+		buffer[i] = '\0';
 }
 
-int main ()
+void printArray(char * array, int size)
 {
-    /*variables*/
-    user_data u1,u2;
-    int sock;
-    unsigned int len;
-    string tmp;
-    char** board = new char*[lines];
-	for (int p=0;p<lines;p++)
-		board[p] = new char[coloumns];
-    /*buffer for messages and data*/
-    char buffer[1024]={'\0'};
-    /*define socket data for both server and client*/
-    struct sockaddr_in serv,client;
-    /*setting listen socket.
-     return -1 if failed, o.w returns socket number*/
-    cout<<"% server log %"<<endl<<endl;
-    if((sock=socket(AF_INET,SOCK_STREAM,0))==-1)
-    {
-        cout<<"--Error: Failed creating socket"<<endl;
-        return 1;
-    }
-    /*sets server socket (to listen) data*/
-    serv.sin_family = AF_INET;
-    serv.sin_port = htons(65535);
-    serv.sin_addr.s_addr = INADDR_ANY;
-    len=sizeof(struct sockaddr_in);
-    /*binding port to listen*/
-    if((bind(sock,(struct sockaddr*)&serv,len))==-1)
-    {
-        cout<<"--Error: Failed to bind"<<endl;
-        return 1;
-    }
-    /*set server to listen to 2 incoming connection*/
-    listen (sock,2);
-    /*accept first connection */
-    cout<<"--waiting for player 1 to connect"<<endl;
-    u1.client_id=accept(sock,(struct sockaddr *)&client ,&len);
-    cout<<"--client connected from port: "<<ntohs(client.sin_port)<<" and ip: "<<inet_ntoa(client.sin_addr)<<endl;
-    sprintf(buffer,"%s",inet_ntoa(client.sin_addr));
-    send(u1.client_id,buffer,sizeof(buffer),0);
-    nullTheArray(buffer);
-    sprintf(buffer,"%s","please choose nickname:");
-    cout<<"--nickname selection sent to user 1, waiting for nick input"<<endl;
-    send(u1.client_id,buffer,sizeof(buffer),0);
-    recv(u1.client_id,buffer,sizeof(buffer),0);
-    u1.username.assign(buffer);
-    cout<<"--player 1 nickname: "<<u1.username<<endl;
-    cout<<"--waiting for player 2 to connect"<<endl;
-    u2.client_id=accept(sock,(struct sockaddr *)&client ,&len);
-    cout<<"--client connected from port: "<<ntohs(client.sin_port)<<" and ip: "<<inet_ntoa(client.sin_addr)<<endl;
-    sprintf(buffer,"%s",inet_ntoa(client.sin_addr));
-    send(u2.client_id,buffer,sizeof(buffer),0);
-    nullTheArray(buffer);
-    sprintf(buffer,"%s","please choose nickname:");
-    cout<<"--nickname selection sent to user 2, waiting for nick input"<<endl;
-    send(u2.client_id,buffer,sizeof(buffer),0);
-    recv(u2.client_id,buffer,sizeof(buffer),0);
-    u2.username.assign(buffer);
-    cout<<"--player 2 nickname: "<<u2.username<<endl;
-    cout<<"--waiting for player 1 to choose color"<<endl;
+	for (int i = 0; i < size; i++)
+		cout << array[i];
+	cout << endl;
+}
 
-    //Player color pick and input control
-    do{
-    	nullTheArray(buffer);
-    	//cout <<"while 1" << endl;
-        //this command will be sent to user 1 only
-        sprintf(buffer,"%s","I");
-        send(u1.client_id,buffer,sizeof(buffer),0);
-    	nullTheArray(buffer);
-        sprintf(buffer,"%s","please insert color (R or B only):");
-        send(u1.client_id,buffer,sizeof(buffer),0);
-        recv(u1.client_id,buffer,sizeof(buffer),0);
-        //command to u1 only end here
-        u1.color = buffer[0];
-        if(u1.color == 'R' || u1.color == 'r' )
-        {
-                u2.color='B';
-                cout<<"--color selected successfully!"<<endl;
-        }
-        else if (u1.color == 'B' || u1.color == 'b' )
-        {
-                u2.color='R';
-                cout<<"--color selected successfully!"<<endl;
-        }
-        else
-        {
-                u2.color='Z';
-                cout<<"--wrong color selected, sending-again"<<endl;
-        }
-    } while (u2.color == 'Z');
+void startConnection(int* sock, char* buffer, sockaddr_in* serv, sockaddr_in* client, user_data * u1, user_data* u2)
+{
 
-    //Start game screen
-    sprintf(buffer,"%s","2");
-    send(u1.client_id,buffer,sizeof(buffer),0);
-    send(u2.client_id,buffer,sizeof(buffer),0);
+}
+
+void initBoard(char* board)
+{
+	for (int p = 0; p < lines*coloumns; p++)
+		board[p] = ' ';
+}
+
+int main()
+{
+	/*variables*/
+	user_data u1, u2;
+	int sock;
+	char buffer[1024] = { '\0' };
+	int firstPlayer;
+	unsigned int len;
+	char board[lines*coloumns];
+	initBoard(board);
+	/*buffer for messages and data*/
+	/*define socket data for both server and client*/
+	struct sockaddr_in serv, client;
+	/*setting listen socket.
+	return -1 if failed, o.w returns socket number*/
+	cout << "% server log %" << endl << endl;
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		cout << "--Error: Failed creating socket" << endl;
+		return 1;
+	}
+	/*sets server socket (to listen) data*/
+	serv.sin_family = AF_INET;
+	serv.sin_port = htons(65535);
+	serv.sin_addr.s_addr = INADDR_ANY;
+	len = sizeof(struct sockaddr_in);
+	/*binding port to listen*/
+	if ((bind(sock, (struct sockaddr*)&serv, len)) == -1)
+	{
+		cout << "--Error: Failed to bind" << endl;
+		return 1;
+	}
+	/*set server to listen to 2 incoming connection*/
+	listen(sock, 2);
+
+
+	/*accept first connection */
+	cout << "--waiting for player 1 to connect" << endl;
+	u1.client_id = accept(sock, (struct sockaddr *)&client, &len);
+	cout << "--client connected from port: " << ntohs(client.sin_port) << " and ip: " << inet_ntoa(client.sin_addr) << endl;
+	sprintf(buffer, "%s", inet_ntoa(client.sin_addr));
+	send(u1.client_id, buffer, sizeof(buffer), 0);
 	nullTheArray(buffer);
-	for (int i=0;i<lines;i++)
-		for (int j=0;j>coloumns;j++)
-			sprintf(buffer,"%c",board[i][j]);
-    send(u1.client_id,buffer,sizeof(buffer),0);
-    send(u2.client_id,buffer,sizeof(buffer),0);
+	sprintf(buffer, "%s", "please choose nickname:");
+	cout << "--nickname selection sent to user 1, waiting for nick input" << endl;
+	send(u1.client_id, buffer, sizeof(buffer), 0);
+	recv(u1.client_id, buffer, sizeof(buffer), 0);
+	u1.username.assign(buffer);
+	cout << "--player 1 nickname: " << u1.username << endl;
+
+	/*accept 2nd connection */
+	cout << "--waiting for player 2 to connect" << endl;
+	u2.client_id = accept(sock, (struct sockaddr *)&client, &len);
+	cout << "--client connected from port: " << ntohs(client.sin_port) << " and ip: " << inet_ntoa(client.sin_addr) << endl;
+	sprintf(buffer, "%s", inet_ntoa(client.sin_addr));
+	send(u2.client_id, buffer, sizeof(buffer), 0);
 	nullTheArray(buffer);
-         
-    
-    //Print all players data -- server side
-    cout << "--users data" << endl;
-    cout << endl << "  user 1:" << endl << "  client id: " << u1.client_id << " nickname: " <<
-            u1.username << " color: " << u1.color << endl << endl;
-    cout << "  user 2:" << endl << "  client id: "<<u2.client_id<<" nickname: "<<
-            u2.username<<" color: "<<u2.color<<endl;
+	sprintf(buffer, "%s", "please choose nickname:");
+	cout << "--nickname selection sent to user 2, waiting for nick input" << endl;
+	send(u2.client_id, buffer, sizeof(buffer), 0);
+	recv(u2.client_id, buffer, sizeof(buffer), 0);
+	u2.username.assign(buffer);
+	cout << "--player 2 nickname: " << u2.username << endl;
+	cout << "--waiting for player 1 to choose color" << endl;
 
+	//Player color pick and input control
+	do{
+		nullTheArray(buffer);
+		//this command will be sent to user 1 only
+		sprintf(buffer, "%s", "I");
+		send(u1.client_id, buffer, sizeof(buffer), 0);
+		nullTheArray(buffer);
+		sprintf(buffer, "%s", "please insert color (R or B only):");
+		send(u1.client_id, buffer, sizeof(buffer), 0);
+		recv(u1.client_id, buffer, sizeof(buffer), 0);
+		//command to u1 only end here
+		u1.color = buffer[0];
+		if (u1.color == 'R' || u1.color == 'r')
+		{
+			u1.color = 'R';
+			u2.color = 'B';
+			cout << "--Color selected successfully!" << endl;
+		}
+		else if (u1.color == 'B' || u1.color == 'b')
+		{
+			u1.color = 'B';
+			u2.color = 'R';
+			cout << "--Color selected successfully!" << endl;
+		}
+		else
+		{
+			u2.color = 'Z';
+			cout << "--Wrong color selected, sending-again" << endl;
+		}
+	} while (u2.color == 'Z');
 
-    /*sends game data to both players*/
-    buffer[0]='M';
-    send(u1.client_id,buffer,sizeof(buffer),0);
-    send(u2.client_id,buffer,sizeof(buffer),0);
-    nullTheArray(buffer);
-    sprintf(buffer,"you are playing against: %s ,your color is: %c\n" ,(char*)u2.username.c_str(),u1.color);
-    send(u1.client_id,buffer,sizeof(buffer),0);
-    nullTheArray(buffer);
-    sprintf(buffer,"you are playing against: %s ,your color is: %c\n" ,(char*)u1.username.c_str(),u2.color);
-    send(u2.client_id,buffer,sizeof(buffer),0);  
-    nullTheArray(buffer);
-          
-    //send an end game message to both players
-    sprintf(buffer,"%s","E");
-    send(u1.client_id,buffer,sizeof(buffer),0);
-    send(u2.client_id,buffer,sizeof(buffer),0); 
+	//Player choose a bit and server XORs the bits and chooses who starts the game
 	nullTheArray(buffer);
-    cout << endl << "% end of log %" << endl;
+	//this command will be sent to user 1 only
+	sprintf(buffer, "%s", "I");
+	send(u1.client_id, buffer, sizeof(buffer), 0);
+	send(u2.client_id, buffer, sizeof(buffer), 0);
+	nullTheArray(buffer);
+	sprintf(buffer, "%s", "please choose 1 or 0\n");
+	send(u1.client_id, buffer, sizeof(buffer), 0);
+	send(u2.client_id, buffer, sizeof(buffer), 0);
 
-    int true1 = 1;
-    setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,&true1,sizeof(int));
- }
+	//receive player one XOR bit
+	buffer[1] = '\0';
+	recv(u1.client_id, buffer, sizeof(buffer), 0);
+	u1.start_bit = buffer[0];
+	while (((u1.start_bit != '0') && (u1.start_bit != '1')) || buffer[1] != '\0')
+	{
+		sprintf(buffer, "%s", "I");
+		send(u1.client_id, buffer, sizeof(buffer), 0);
+		sprintf(buffer, "%s", "Choose a valid value (0 or 1)\n");
+		send(u1.client_id, buffer, sizeof(buffer), 0);
+		buffer[1] = '\0';
+		recv(u1.client_id, buffer, sizeof(buffer), 0);
+		u1.start_bit = buffer[0];
+	}
+
+	//receive player 2 XOR bit
+	buffer[1] = '\0';
+	recv(u2.client_id, buffer, sizeof(buffer), 0);
+	u2.start_bit = buffer[0];
+	while (((u2.start_bit != '0') && (u2.start_bit != '1')) || buffer[1] != '\0')
+	{
+		sprintf(buffer, "%s", "I");
+		send(u2.client_id, buffer, sizeof(buffer), 0);
+		sprintf(buffer, "%s", "Choose a valid value (0 or 1)\n");
+		send(u2.client_id, buffer, sizeof(buffer), 0);
+		buffer[1] = '\0';
+		recv(u2.client_id, buffer, sizeof(buffer), 0);
+		u2.start_bit = buffer[0];
+	}
+	//calculate
+	u1.start_bit = u1.start_bit - '0';
+	u2.start_bit = u2.start_bit - '0';
+	firstPlayer = u1.start_bit ^ u2.start_bit;
+
+
+	//Start game screen
+	sprintf(buffer, "%s", "2");
+	send(u1.client_id, buffer, sizeof(buffer), 0);
+	send(u2.client_id, buffer, sizeof(buffer), 0);
+	nullTheArray(buffer);
+	strcpy(buffer, board);
+	send(u1.client_id, buffer, sizeof(buffer), 0);
+	send(u2.client_id, buffer, sizeof(buffer), 0);
+	nullTheArray(buffer);
+
+
+	//Print all players data -- server side
+	cout << "--users data" << endl;
+	cout << endl << "  user 1:" << endl << "  client id: " << u1.client_id << " nickname: " <<
+		u1.username << " color: " << u1.color << endl << endl;
+	cout << "  user 2:" << endl << "  client id: " << u2.client_id << " nickname: " <<
+		u2.username << " color: " << u2.color << endl;
+	cout << (firstPlayer ? "--Player 2 starts" : "--Player 1 starts") << endl;
+
+	/*sends game data to both players*/
+	buffer[0] = 'M';
+	send(u1.client_id, buffer, sizeof(buffer), 0);
+	send(u2.client_id, buffer, sizeof(buffer), 0);
+	nullTheArray(buffer);
+	sprintf(buffer, "You are playing against: %s ,your color is: %c\nPlayer %d starts\n", (char*)u2.username.c_str(), u1.color, (1 + firstPlayer));
+	send(u1.client_id, buffer, sizeof(buffer), 0);
+	nullTheArray(buffer);
+	sprintf(buffer, "You are playing against: %s ,your color is: %c\nPlayer %d starts\n", (char*)u1.username.c_str(), u2.color, (1 + firstPlayer));
+	send(u2.client_id, buffer, sizeof(buffer), 0);
+	nullTheArray(buffer);
+
+	//send an end game message to both players
+	sprintf(buffer, "%s", "E");
+	send(u1.client_id, buffer, sizeof(buffer), 0);
+	send(u2.client_id, buffer, sizeof(buffer), 0);
+	nullTheArray(buffer);
+	cout << endl << "% end of log %" << endl;
+
+	int true1 = 1;
+	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &true1, sizeof(int));
+}
